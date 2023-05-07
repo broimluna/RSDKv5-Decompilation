ab7213e93c621009c5cabb48d51f07e6378d3b80
Mefiresu <15063879+Mefiresu@users.noreply.github.com>
2023-05-07 08:30:21 +0200

    wii: Disable editor code through cmake variable

--- platforms/NintendoWii.cmake
+++ platforms/NintendoWii.cmake
@@ -6,12 +6,12 @@ set(RETRO_SUBSYSTEM "GX" CACHE STRING "The subsystem to use")
 if(NOT (RETRO_SUBSYSTEM STREQUAL "GX"))
     message(FATAL_ERROR "RETRO_SUBSYSTEM must be set to GX")
 endif()
-if(NOT GAME_STATIC) 
+if(NOT GAME_STATIC)
     message(FATAL_ERROR "GAME_STATIC must be on")
 endif()
 
 # pkg_check_modules(OGG ogg)
-# 
+#
 # if(NOT OGG_FOUND)
 #     set(COMPILE_OGG TRUE)
 #     message(NOTICE "libogg not found, attempting to build from source")
@@ -21,9 +21,9 @@ endif()
 #     target_link_options(RetroEngine PRIVATE ${OGG_STATIC_LDLIBS_OTHER})
 #     target_compile_options(RetroEngine PRIVATE ${OGG_STATIC_CFLAGS})
 # endif()
-# 
+#
 # pkg_check_modules(THEORA theora theoradec)
-# 
+#
 # if(NOT THEORA_FOUND)
 #     message("could not find libtheora, attempting to build manually")
 #     set(COMPILE_THEORA TRUE)
@@ -34,8 +34,10 @@ endif()
 #     target_compile_options(RetroEngine PRIVATE ${THEORA_STATIC_CFLAGS})
 # endif()
 
+set(GAME_INCLUDE_EDITOR OFF CACHE BOOL "Disable unused editor code" FORCE)
+
 set(SHARED_COMPILE
-    $<$<COMPILE_LANGUAGE:CXX>:-fno-rtti -fno-threadsafe-statics> 
+    $<$<COMPILE_LANGUAGE:CXX>:-fno-rtti -fno-threadsafe-statics>
     -Os -fomit-frame-pointer -ffunction-sections -fdata-sections -fno-asynchronous-unwind-tables -fmerge-all-constants
 )
 
@@ -49,7 +51,6 @@ target_link_options(RetroEngine PRIVATE ${SHARED_LINK})
 target_link_libraries(RetroEngine fat aesnd)
 
 target_compile_options(${GAME_NAME} PRIVATE ${SHARED_COMPILE})
-target_compile_definitions(${GAME_NAME} PRIVATE RETRO_INCLUDE_EDITOR=0)
 target_link_options(${GAME_NAME} PRIVATE ${SHARED_LINK})
 
 set(PLATFORM Wii)
